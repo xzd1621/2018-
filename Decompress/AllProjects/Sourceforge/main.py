44f15b559aa2f07d57e933b057382453529c277f
xuzhida <1459601448@qq.com>
2018-09-04 14:10:10 +0800

    delete some files

--- Decompress/AllProjects/Sourceforge/main.py
+++
@@ -1,7 +0,0 @@
-
-from scrapy import cmdline
-
-if __name__ == '__main__':
-    #cmdline.execute("scrapy crawl OschinaSpider".split())
-    cmdline.execute("scrapy crawl sourceforge".split())
-    #cmdline.execute("scrapy runspider ./spiders/GolangSpider.py".split())
--- Decompress/AllProjects/scfile/main.py
+++
@@ -1,6 +0,0 @@
-
-from scrapy import cmdline
-
-if __name__ == '__main__':
-    #cmdline.execute("scrapy crawl OschinaSpider".split())
-    cmdline.execute("scrapy crawl downloadfiles".split())
